eaee14fbbb994898dcf37519b9d0b1d4b57b18d2
Eberson <eberson.oliveira@gmail.com>
2025-03-20 19:35:51 -0300

    removed unnecessary code

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,3 @@
-import 'package:base_app/pages/about/about_page_widget.dart';
-import 'package:base_app/pages/home/home_page_widget.dart';
-
 import 'nav.dart';
 
 List<FFRoute> routes = [
@@ -10,14 +7,4 @@ List<FFRoute> routes = [
   //   builder: (context, _) =>
   //       appStateNotifier.loggedIn ? const NavBarPage() : const LoginPageWidget(),
   // ),
-  FFRoute(
-    name: 'AboutPage',
-    path: '/about',
-    builder: (context, params) => const AboutPageWidget(),
-  ),
-  FFRoute(
-    name: 'HomePage',
-    path: '/',
-    builder: (context, params) => const HomePageWidget(),
-  ),
 ];
